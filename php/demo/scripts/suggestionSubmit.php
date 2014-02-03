95ae5b476353fc4ba018b3e6f2e6ca605c8f2531
Matt <projectvariorum@gmail.com>
2014-02-03 13:37:14 -0500

    Experimenting with mail

--- php/demo/scripts/suggestionSubmit.php
+++ php/demo/scripts/suggestionSubmit.php
@@ -1,13 +1,37 @@
 <?php
 error_reporting(0); 
+require "Mail.php";
 header('Content-Type: text/plain; charset=utf-8');
-$submission = $_GET["sug"];
-$code = $_GET["code"];
+$submission = "TEST";//$_GET["sug"];
+$subj = "TEST";//$_GET["code"];
+$sender    = "projectvariorum@gmail.com";
+$recipient = "projectvariorum@gmail.com";
+   $server   = "ssl://smtp.gmail.com";
+   $username = "projectvariorum@gmail.com";
+   $password = "estebar1";
+   $port     = "465";
 $test = false;
-if (!is_null($submission) && !is_null($code)) {
+ $headers = array(
+      "From"    => $sender,
+      "To"      => $recipient,
+      "Subject" => $subj
+   );
+   
+    $smtp = Mail::factory("smtp",
+      array(
+        "host"     => $server,
+        "username" => $username,
+        "password" => $password,
+        "auth"     => true,
+        "port"     => 465
+      )
+   );
+   $mail = $smtp->send($recipient, $headers, $submission);
+
+/*if (!is_null($submission) && !is_null($code)) {
 $address = "projectvariorum@gmail.com";
 $test = mail($address, $code, $submission);
 if ($test==false) { echo 0; }
 else { echo 1; }
-}
+}*/
 ?>
