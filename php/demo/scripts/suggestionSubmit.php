567b8243c2da329c66e3738191ed8a2fcd2a70b6
Matt <projectvariorum@gmail.com>
2014-02-03 15:46:29 -0500

    New encoded file

--- php/demo/scripts/suggestionSubmit.php
+++ php/demo/scripts/suggestionSubmit.php
@@ -2,13 +2,13 @@
 error_reporting(0); 
 require "Mail.php";
 header('Content-Type: text/plain; charset=utf-8');
-$submission = "TEST";//$_GET["sug"];
-$subj = "TEST";//$_GET["code"];
-$sender    = "projectvariorum@gmail.com";
-$recipient = "projectvariorum@gmail.com";
+$submission = $_GET["sug"];
+$subj = $_GET["code"];
+$sender    = "projectvariorum+sender@gmail.com";
+$recipient = "projectvariorum+recipient@gmail.com";
    $server   = "ssl://smtp.gmail.com";
    $username = "projectvariorum@gmail.com";
-   $password = "estebar1";
+   $password = file_get_contents('../../../../ekey');
    $port     = "465";
 $test = false;
  $headers = array(
@@ -28,10 +28,10 @@ $test = false;
    );
    $mail = $smtp->send($recipient, $headers, $submission);
 
-/*if (!is_null($submission) && !is_null($code)) {
-$address = "projectvariorum@gmail.com";
-$test = mail($address, $code, $submission);
-if ($test==false) { echo 0; }
-else { echo 1; }
-}*/
+   if (PEAR::isError($mail)) {
+      echo 0;
+   }
+   else {
+	echo 1;
+	}
 ?>
